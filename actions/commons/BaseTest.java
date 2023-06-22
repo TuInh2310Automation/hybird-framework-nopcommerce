4fa8d309907cb2714cd183b462167fb0ddd92e28
TuInh2310Automation <tuinhle2310@gmail.com>
2023-06-22 17:02:24 +0700

    Running on headless mode

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -2,8 +2,10 @@ package commons;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BaseTest {
 	private WebDriver driver;
@@ -14,12 +16,24 @@ public class BaseTest {
 			System.setProperty("webdriver.chrome.driver", projectPathString + "\\browserDrivers\\chromedriver.exe");
 			driver = new ChromeDriver();
 			break;
-
+		case "h_chrome":
+			System.setProperty("webdriver.chrome.driver", projectPathString + "\\browserDrivers\\chromedriver.exe");
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("-headless");
+			chromeOptions.addArguments("window-size=1920x1080");
+			driver = new ChromeDriver(chromeOptions);
+			break;
 		case "firefox":
 			System.setProperty("webdriver.gecko.driver", projectPathString + "\\browserDrivers\\geckodriver.exe");
 			driver = new FirefoxDriver();
 			break;
-
+		case "h_firefox":
+			System.setProperty("webdriver.gecko.driver", projectPathString + "\\browserDrivers\\geckodriver.exe");
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			firefoxOptions.addArguments("-headless");
+			firefoxOptions.addArguments("window-size=1920x1080");
+			driver = new FirefoxDriver(firefoxOptions);
+			break;
 		case "edge":
 			System.setProperty("webdriver.edge.driver", projectPathString + "\\browserDrivers\\msedgedriver.exe");
 			driver = new EdgeDriver();
